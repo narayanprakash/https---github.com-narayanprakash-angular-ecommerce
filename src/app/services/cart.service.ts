111efdc11bb4d7b17d6f0c50cec17b06e111ab14
narayanprakash <prakashnarayan001@gmail.com>
2024-11-26 01:30:41 +0530

    localSession impl

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -11,7 +11,7 @@ export class CartService {
 
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
-storage : Storage = sessionStorage;
+storage : Storage = localStorage;
   constructor() { 
     let data = JSON.parse(this.storage.getItem('cartItems'));
     if(data!=null){
